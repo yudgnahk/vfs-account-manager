c6584337a0db993023dda30830c67aa4fc9714ad
Kelvin Ha <haleduykhang@gmail.com>
2024-04-01 15:58:06 +0700

    Add ping router

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "pong")
+	})
+
+	fmt.Println("Server is running on http://localhost:8080/ping")
+	http.ListenAndServe(":8080", nil)
 }
